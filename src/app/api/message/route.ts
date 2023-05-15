665eba75d7a38d9ab7f695ab6772db37a2fa986b
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2023-05-15 15:13:27 -0300

    feat: implement of real-time response streaming

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -1,5 +1,5 @@
 import { chatbotPrompt } from "@/helpers/constants/chatbot-prompt"
-import { ChatGPTMessage } from "@/lib/openai-stream"
+import { ChatGPTMessage, OpenAIStream, OpenAIStreamPayload } from "@/lib/openai-stream"
 import { MessageArraySchema } from "@/lib/validators/message"
 
 export async function POST(req: Request,) {
@@ -16,4 +16,20 @@ export async function POST(req: Request,) {
         role: 'system',
         content: chatbotPrompt
     })
+
+    const payload: OpenAIStreamPayload = {
+        model: 'gpt-3.5-turbo',
+        messages: outboundMessages,
+        temperature: 0.4,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+        max_tokens: 150,
+        stream: true,
+        n: 1
+    }
+
+    const stream = await OpenAIStream(payload)
+
+    return new Response(stream)
 }
