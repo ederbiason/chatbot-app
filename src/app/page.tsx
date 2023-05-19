429296a1b496a65fa17862d96774bc4d7d8a8fd7
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2023-05-19 15:32:36 -0300

    feat: book page scratch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+import BookSection from "@/components/BookSection";
 
 export default function Home() {
   return (
-    <main className="absolute inset-0 flex justify-center items-center">
-      website content
+    <main className="absolute inset-0 mx-auto px-4 max-w-7xl">
+      <div className="flex gap-2 items-end pt-12 pb-7">
+        <p className="text-2xl text-blue-600 hover:underline hover:underline-offset-2 hover:text-blue-700">Books to Scrape</p>
+        <p className="text-lg text-zinc-500">We love being scraped!</p>
+      </div>
+
+      <BookSection />
     </main>
   )
 }
---
+++ src/components/BookSection.tsx
@@ -0,0 +1,20 @@
+
+const BookSection = () => {
+  return (
+    <div className="bg-white w-full p-7 rounded-md">
+        {/* <BooksHeader /> */}
+
+        <div className="flex w-full">
+            <div className="bg-zinc-200 p-5 rounded-md border border-zinc-400 shadow-sm">
+                
+            </div>
+
+            <div className="flex-1 bg-blue-200 ">
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default BookSection
